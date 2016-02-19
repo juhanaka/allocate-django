1b4d2dc790fd72eab685cc1ab1d975534040c4a2
Juhana Kangaspunta <juhana.kangaspunta@gmail.com>
2016-02-19 08:33:09 -0800

    Company model

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from authentication import models
+from django.contrib.auth import models as auth_models
 
 admin.site.register(models.OutlookCredentialsModel)
 admin.site.register(models.RescuetimeTokenModel)
+admin.site.register(auth_models.Permission)
+admin.site.register(models.CompanyModel)
